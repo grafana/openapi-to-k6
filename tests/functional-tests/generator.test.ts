342f8aa1864860b4e132d81e804a204637ae8eca
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2024-11-13 14:58:36 +0000

    Add unit tests

--- tests/functional-tests/generator.test.ts
+++ tests/functional-tests/generator.test.ts
@@ -317,3 +317,94 @@ describe('generator', () => {
     })
   })
 })
+
+describe('validate sample k6 script', async () => {
+  const schemaNameToAssertionsMap = {
+    'schema_with_examples.json': {
+      expected_string: [
+        `let id, postItemsIdBody, putItemsIdBody, patchItemsIdBody, postItemsFormUrlEncodedBody, postItemsFormDataBody, params, userId, postId, headers;`,
+        `id = "12345-getItemById";`,
+        `id = "123450-createItemById";`,
+        `id = "123450-updateItemById";`,
+        `id = "123450-patchItemById";`,
+        `id = "123450-deleteItemById";`,
+        `id = "123450-checkItemById";`,
+        `userId = "`, // This is generated as random word using faker hence to validating the value
+        `postId = "`, // This is generated as random word using faker hence to validating the value
+        `postItemsIdBody = { name: "Sample Item", description: "This is a sample description for the item.", };`,
+        `putItemsIdBody = { name: "Updated Item Name", description: "Updated description for the item.", };`,
+        `patchItemsIdBody = { name: "Partially Updated Item Name", };`,
+        `postItemsFormUrlEncodedBody = { name: "Form Encoded Item", description: "Description for form-urlencoded item.", };`,
+        `postItemsFormDataBody = { name: "Form Data Item", };`,
+        `params = { id: "123450-getItemByIdWithHeader", };`,
+        `params = { itemId: "67890", };`,
+        `headers = { "X-Auth-Token": "Bearer abcdef12345", "X-Correlation-ID": "correlation-12345", };`,
+      ],
+    },
+    'schema_using_ref_models.json': {
+      expected_string: [
+        `let id, itemBase, updatedItem, patchItem, formUrlEncodedItem, formDataItem, params, headers, userId, postId;`,
+        `id = "12345-getItemById";`,
+        `userId = "`, // This is generated as random word using faker hence to validating the value
+        `postId = "`, // This is generated as random word using faker hence to validating the value
+        `itemBase = { name: "Sample Item", description: "This is a sample description for the item.", };`,
+        `updatedItem = { name: "Updated Item Name", description: "Updated description for the item.", };`,
+        `patchItem = { name: "Partially Updated Item Name", };`,
+        `formUrlEncodedItem = { name: "Form Encoded Item", description: "Description for form-urlencoded item.", };`,
+        `formDataItem = { name: "Form Data Item", };`,
+        `params = { id: "123450-getItemByIdWithHeader", };`,
+        `headers = { "X-Client-ID": "client-123", };`,
+        `params = { itemId: "67890", };`,
+        `headers = { "X-Auth-Token": "Bearer abcdef12345", "X-Correlation-ID": "correlation-12345", };`,
+      ],
+    },
+  }
+
+  for (const [schemaName, assertions] of Object.entries(
+    schemaNameToAssertionsMap
+  )) {
+    describe(`test ${schemaName} schema`, () => {
+      let tempDir: string, openapiSchemaPath: string
+
+      beforeAll(async () => {
+        tempDir = await mkdtemp(
+          path.join(os.tmpdir(), `example-k6-script-test-${schemaName}`)
+        )
+        openapiSchemaPath = path.join(tempDir, 'openapi-schema.json')
+        await writeFile(
+          openapiSchemaPath,
+          JSON.stringify(loadFixture(schemaName))
+        )
+      })
+
+      it('should generate sample k6 script with correct example values', async () => {
+        const generatedClientPath = path.join(tempDir, `generated-client`)
+
+        await generator({
+          openApiPath: openapiSchemaPath,
+          outputDir: generatedClientPath,
+          mode: Mode.SINGLE,
+          shouldGenerateSampleK6Script: true,
+        })
+
+        const generatedFiles = fs.readdirSync(generatedClientPath)
+        expect(generatedFiles.length).toBe(2)
+
+        // Get the k6 script file
+        const k6ScriptFile = generatedFiles.find((file) =>
+          file.includes('k6-script.sample.ts')
+        )
+        expect(k6ScriptFile).toBeDefined()
+
+        const k6ScriptFilePath = path.join(generatedClientPath, k6ScriptFile!)
+        const k6ScriptContent = await readFile(k6ScriptFilePath, 'utf-8')
+
+        for (const expectedString of assertions.expected_string) {
+          expect(
+            replaceSpacesAndNewLineToSingleSpace(k6ScriptContent)
+          ).toContain(expectedString)
+        }
+      })
+    })
+  }
+})
