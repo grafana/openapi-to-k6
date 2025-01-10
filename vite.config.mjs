f695b539a1c3b5582790557484e44764e94d4573
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2025-01-10 13:49:50 +0000

    Update and fix unit tests

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: 'node',
+    globals: true,
+    include: ['tests/**/*.test.ts'],
     coverage: {
       reporter: ['json', 'text', 'lcov'],
       reportsDirectory: 'coverage',
